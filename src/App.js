09786d7caea1ec800d5270c09631fcd08250225d
Jana-SI <janaina.adolfo.ds@gmail.com>
2023-07-17 16:30:30 -0300

    Estruturando o projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Empresa from "./components/pages/Empresa";
+import Contato from "./components/pages/Contato";
+import NovoProjeto from "./components/pages/NovoProjeto";
+import Container from "./components/layout/Container";
+
 function App() {
   return (
-    <div className="App">
-      <p>costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Pagina Principal</Link>
+        <Link to="/empresa">Empresa</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/novoprojeto">Novo Projeto</Link>
+      </div>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/empresa" element={<Empresa />} />
+          <Route exact path="/contato" element={<Contato />} />
+          <Route exact path="/novoprojeto" element={<NovoProjeto />} />
+        </Routes>
+      </Container>      
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ src/components/pages/NovoProjeto.js
@@ -0,0 +1,5 @@
+function NovoProjeto() {
+    return <h1>NovoProjeto</h1>
+}
+
+export default NovoProjeto
