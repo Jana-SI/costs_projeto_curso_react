f65ba9e8e6b792171c99a08929f8b11a4de072e0
Jana-SI <janaina.adolfo.ds@gmail.com>
2023-07-21 18:00:20 -0300

    Página para o formulário de projeto

--- src/components/pages/NovoProjeto.js
+++ src/components/pages/NovoProjeto.js
@@ -1,5 +1,14 @@
+import ProjetoForm from '../project/ProjetoForm'
+import styles from './NovoProjeto.module.css'
+
 function NovoProjeto() {
-    return <h1>NovoProjeto</h1>
+    return(
+        <div className={styles.novoProjeto_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjetoForm />
+        </div>
+    )
 }
 
 export default NovoProjeto
