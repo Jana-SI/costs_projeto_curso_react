950a1c5e74c2bb1835704eb6baae69d9ca74ba6c
Jana-SI <janaina.adolfo.ds@gmail.com>
2023-08-02 15:37:15 -0300

    Exibindo detalhes do projeto

--- src/components/pages/Projeto.js
+++ src/components/pages/Projeto.js
@@ -1,14 +1,20 @@
 import { useParams } from 'react-router-dom'
-import styles from './Projeto.module.css'
 import { useEffect, useState } from 'react'
+
+import styles from './Projeto.module.css'
+
+import Carregamento from '../../components/layout/Carregamento'
+import Container from '../../components/layout/Container'
 
 function Projeto() {
 
   const { id } = useParams()
   const [projeto, setProjeto] = useState([])
+  const [mostraProjetoForm, setMostraProjetoForm] = useState(false)
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projetos/${id}`, {
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projetos/${id}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -18,14 +24,48 @@ function Projeto() {
         setProjeto(data)
       })
       .catch((err) => console.log(err))
+    }, 300)
   }, [id])
 
+  function toggleProjetoForm() {
+    setMostraProjetoForm(!mostraProjetoForm)
+  }
+
   return (
-    <div>
-      {projeto && (
-        <p>Projeto {projeto.nomeP}</p>
+    <>
+      {projeto.nomeP ? (
+        <div className={styles.projeto_detalhes}>
+          <Container customClass="column"></Container>
+          <div className={styles.detalhes_container}>
+            <h1>Projeto: {projeto.nomeP}</h1>
+
+            <button className={styles.btn} onClick={toggleProjetoForm}>
+              {!mostraProjetoForm ? 'Editar Projeto' : 'Fechar'}
+            </button>
+
+            {!mostraProjetoForm ? (
+              <div className={styles.projeto_info}>
+                <p>
+                  <span>Categoria:</span> {projeto.categorias.name}
+                </p>
+                <p>
+                  <span>Total de Orçamento:</span> {projeto.valorT}
+                </p>
+                <p>
+                  <span>Total Utilizado:</span> R$ {projeto.cost}
+                </p>
+              </div>
+              ) : (
+                <div className={styles.projeto_info}>
+                  <p>from</p>
+                </div>
+              )}
+          </div>
+        </div>
+      ) : (
+        <Carregamento />
       )}
-    </div>
+    </>
   )
 }
 
